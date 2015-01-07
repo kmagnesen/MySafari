92b30656287d54ec1b575d384663d3cb4d73fd80
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-07 14:37:29 -0600

    Just Started

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,20 +8,63 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 
 @end
 
 @implementation ViewController
+
+
+- (IBAction)onStopLoadingButtonPressed:(id)sender {
+}
+
+- (IBAction)onForwardButtonPressed:(id)sender {
+}
+
+- (IBAction)onBackButtonPressed:(id) sender {
+
+}
+
+- (void) stopLoading {
+    
+}
+
+- (void) goBack{
+
+}
+
+- (void) goForward{
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)loadURL:(NSString *)urlString {
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:urlRequest];
 }
+
+-(void)webViewDidStartLoad:(UIWebView *)webView{
+    [self.spinner startAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    [self.spinner stopAnimating];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self loadURL: textField.text];
+    return YES;
+}
+
+
+
 
 @end
